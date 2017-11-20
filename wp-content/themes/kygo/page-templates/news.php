fe0d4b8369e052ce06711ecb61b6a2199538ba10
YoannBa <yoann.baunach@hetic.net>
2017-11-20 22:33:46 +0100

    :sparkles: news > create news page

--- wp-content/themes/kygo/page-templates/news.php
+++ wp-content/themes/kygo/page-templates/news.php
@@ -2,4 +2,51 @@
 /*
  Template Name: News
  */
+    get_header();
+
+    if (have_posts()) :
+        while (have_posts()) :
+            the_post();
 ?>
+
+<div class="news-container">
+    <div class="news-header">
+
+        <?php
+            $args = array(
+                'post_type' => 'post',
+            );
+
+            $the_query = new WP_Query($args);
+
+            if ($the_query->have_posts()):
+                while ($the_query->have_posts()):
+                    $the_query->the_post();
+        ?>
+
+        <div class="news-header__left">
+            <div class="news-header__image-deco"></div>
+            <img class="news-header__image" src="<?= get_the_post_thumbnail_url(); ?>" alt="thumbnail">
+        </div>
+
+        <div class="news-header__right">
+            <h2 class="news-header__title"><?php the_title(); ?></h2>
+            <div class="news-header__description"><?php the_field('description') ?></div>
+            <div class="news-header__date"><?php the_date(); ?></div>
+            <a class="news-header__more btn-kygo" href="<?php the_permalink(); ?>"></a>
+        </div>
+
+        <?php
+                endwhile;
+            endif;
+        ?>
+
+    </div>
+</div>
+
+<?php
+        endwhile;
+    endif;
+?>
+
+<?php get_footer(); ?>
