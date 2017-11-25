c7adf24acd25c9fffe9c334f3b82359b570747d6
lucdandrel <luc.dandrel@hetic.net>
2017-11-25 17:55:44 +0100

    :sparkles: new single page article

--- wp-content/themes/kygo/single-post.php
+++ wp-content/themes/kygo/single-post.php
@@ -4,10 +4,19 @@
 
         <section class="single-new">
 
+            <div class="single-new__intro">
+                <h2><?php the_title(); ?></h2>
+                <p><?php the_field('description') ?></p>
+                <div class="date"><?= strftime("%d %b. %Y",strtotime(get_post()->post_date)); ?></div>
+            </div>
+
+            <div class="single-new__img">
+                <img src="<?= get_the_post_thumbnail_url(); ?>" alt="">
+            </div>
+
 
         </section>
 
-    <?php the_title(); ?>
     <?php the_content(); ?>
     <?php echo get_the_date(); ?>
 
