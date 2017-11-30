6beb1d5a90b808746e34723487bccc2131e2527a
lucdandrel <luc.dandrel@hetic.net>
2017-11-30 14:47:40 +0100

    :sparkles: new shortcodes

--- wp-content/themes/kygo/page-templates/music.php
+++ wp-content/themes/kygo/page-templates/music.php
@@ -10,7 +10,17 @@ get_header();
     <div class="header-music__img">
         <img class="news-header__image" src="<?= get_the_post_thumbnail_url(); ?>" alt="kygo">
     </div>
+    <div class="header-music__content">
+        <div>
+            <h2><?php the_field('artist_name', 'option'); ?></h2>
+            <p><?php the_field('artist_description', 'option'); ?></p>
+        </div>
+    </div>
 </section>
+
+<div class="theme">
+
+</div>
 
 
 <?php get_footer();
